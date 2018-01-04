3e5c772afebffa288b92ee9231ce9f3e2d9ef3fd
root <1586706912@qq.com>
2018-01-04 19:22:37 +0800

    sfsa

---
+++ jybseconddocker/ownCloud/config.php
@@ -0,0 +1,17 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocjcsvcgz36r',
+  'passwordsalt' => 'tANdePzg+kuxTmSsConNxeWVPlcoB5',
+  'secret' => 'H7HbthjWi8dWXJzA8/15suaTt6L+R3VqBrxd8CFA8/fuNmOu',
+  'trusted_domains' => 
+  array (
+    0 => 'sinzuo.cn:9095',
+    1 => 'www.sinzuo.cn:9095',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://sinzuo.cn:9095',
+  'dbtype' => 'sqlite3',
+  'version' => '10.0.4.4',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
